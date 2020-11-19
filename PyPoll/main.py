17d67fec269265cc38e738e1f72d3198911894cd
GrimmettDS <davidsgrimmett@gmail.com>
2020-11-19 18:13:00 -0500

    initial setup for PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_data = os.path.join("Resources","election_data.csv")
+
+VoteTotal
+Candidates = []
+VotePercent
+EachCanVote
