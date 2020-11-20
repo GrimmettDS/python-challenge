ecce141b6550018ece789ef5d19ab9b7d93e9717
GrimmettDS <davidsgrimmett@gmail.com>
2020-11-19 20:23:22 -0500

    Get vote total...or die trying

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,9 +2,25 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-budget_data = os.path.join("Resources","election_data.csv")
+election_data = os.path.join("Resources","election_data.csv")
 
-VoteTotal
+VoteTotal = 0
 Candidates = []
 VotePercent
 EachCanVote
+
+with open(election_data, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        if row[2] in Candidates
+            VoteTotal += 1
+        else:
+            Candidates.append(row[2])
+            VoteTotal += 1
+
+
+print("Election Results")
+print("----------------------------")
+print(f"{VoteTotal}")
