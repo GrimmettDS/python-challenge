36dabbca6eb24640a1ec7b09ade5bc55a3007567
GrimmettDS <davidsgrimmett@gmail.com>
2020-11-19 21:00:29 -0500

    Stuck midway.  Stuck in loops.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,19 +6,30 @@ election_data = os.path.join("Resources","election_data.csv")
 
 VoteTotal = 0
 Candidates = []
-VotePercent
+VotePercent = []
 EachCanVote
 
+# Read the CSV and split at the commas
 with open(election_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
+    i = 0
+
+    # Candidate Listing and Vote Count loop
     for row in csvreader:
         if row[2] in Candidates
             VoteTotal += 1
         else:
             Candidates.append(row[2])
             VoteTotal += 1
+
+    CandidateLength = len(Candidates)
+        VoteCount = [0] * CandidateLength
+
+    for i in range(CandidateLength)
+        if row[2] == Candidates[i]
+            VoteCount[i] += 1
 
 
 print("Election Results")
